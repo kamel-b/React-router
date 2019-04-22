47a992330a968162e11909df55b27ce70f4737a9
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-04-22 17:16:10 +0200

    first commit

---
+++ src/History.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from 'react';
+
+
+class History extends Component {
+    render () {
+        return (
+
+            <div><p> Une histoire d'amour de routeur </p></div>
+        )
+    }
+};
+export default History;
